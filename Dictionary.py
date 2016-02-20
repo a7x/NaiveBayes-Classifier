6c7541dda4cf4ae02872860c66a8292c00b92fef
Rohit Dholakia <rdholaki@sfu.ca>
2016-02-19 22:40:26 -0800

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -10,16 +10,17 @@ import sys
 dictionary = defaultdict(int)
 
 
-with open(sys.argv[2], 'w') as fdict:
-	for root, dirnames, filenames in os.walk(sys.argv[1]):
-		for d in dirnames:
-			for f in os.listdir(os.path.join(root, d)):
-				with open(os.path.join(sys.argv[1], d, f)) as data:
-					for line in data:
-						words = line.split()
-						for w in words:
-							dictionary[w] += 1
+
+for root, dirnames, filenames in os.walk(sys.argv[1]):
+	for d in dirnames:
+		for f in os.listdir(os.path.join(root, d)):
+			with open(os.path.join(sys.argv[1], d, f)) as data:
+				for line in data:
+					words = line.split()
+					for w in words:
+						dictionary[w] += 1
 	dictionary = OrderedDict(sorted(dictionary.items(),key = lambda t: t[1]))
+with open(sys.argv[2], 'w') as fdict:
 	for k in dictionary:
 		fdict.write("{} {} \n".format(k, str(dictionary[k])))
 
